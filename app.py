4d88314856015a0c182cd622b2b86a787bb8fcb9
gokul153 <dgkgokul15@gmail.com>
2025-04-30 07:31:12 +0530

    intial_commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import openai 
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
