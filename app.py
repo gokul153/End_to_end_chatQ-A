f845e6bd4ffa000b4f8d25fcbdd19b253887d571
gokul153 <freevdi15@gmail.com>
2025-05-02 21:53:04 +0530

    added code until section 148

--- app.py
+++ app.py
@@ -8,3 +8,42 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+### langsmith tracking
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
+os.environ["LANGCHAIN_PROJECT"] = "Simplet chatbot"
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+
+## Prompt template
+prompt = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant that answers questions please repond as you are my teacher."),
+    ("human", "Question:{question}"),
+])
+
+def generate_response(question,api_key,llm,temperature,max_tokens):
+    # Set the OpenAI API key
+    openai.api_key = api_key
+    llm=ChatOpenAI(model=llm)
+    output_parser = StrOutputParser()
+    chain = prompt|llm|output_parser
+    answer = chain({"question": question})
+    return answer
+
+    # Create a ChatOpenAI instance with the specified parameters
+    chat = ChatOpenAI(
+        model="gpt-3.5-turbo",
+        temperature=temperature,
+        max_tokens=max_tokens,
+        openai_api_key=api_key,
+    )
+
+    # Create a prompt template and format it with the question
+    formatted_prompt = prompt.format_messages(question=question)
+
+    # Generate a response using the LLM
+    response = llm(formatted_prompt)
+    
+    return response
+
+
+    
